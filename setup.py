abf2b0a196cf21cbf6122810a3787f08acb0dfb0
Aniruddha Kumar <110346786+Demon-2-Angel@users.noreply.github.com>
2024-07-07 01:36:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+# Read the content of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='Money_Control_Scrap',
     version='0.1.0',
     author="Aniruddha Kumar",
     author_email="foraniruddhakumar@gmail.com",
-    description="A package to install and scrap the money control website into an Excel file to get the links and the headlines of the Financial News",
+    description="A package to scrape news from Money Control",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'selenium',
