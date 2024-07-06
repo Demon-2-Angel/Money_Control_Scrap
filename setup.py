87c6d1216e8777003812f4c585ec152b1df05dd3
Aniruddha Kumar <110346786+Demon-2-Angel@users.noreply.github.com>
2024-07-07 00:48:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ setup(
     version='0.1.0',
     author="Aniruddha Kumar",
     author_email="foraniruddhakumar@gmail.com",
-<<<<<<< HEAD
     description="A package to install and scrap the money control website into an Excel file to get the links and the headlines of the Financial News",
-=======
-    description="A package to install and scrap the money control website into an Excel file to get the links and the headlines of the Financial News"
->>>>>>> 24767de497289813a0bf94c2c50bb71a4d2f29ee
     packages=find_packages(),
     install_requires=[
         'selenium',
